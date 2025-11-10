232b8edc029a1881c2c003b3f0e81af6e0c064a1
shuvamshr <shuvamshr@outlook.com>
2025-11-11 10:09:37 +1100

    Initial Commit

---
+++ Hourish/HourishApp.swift
@@ -0,0 +1,17 @@
+//
+//  HourishApp.swift
+//  Hourish
+//
+//  Created by Shuvam Shrestha on 11/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct HourishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
